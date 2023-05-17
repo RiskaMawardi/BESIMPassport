7391305cbae32e7539fbf1357c4bfdae15628188
RiskaMawardi <riskamawardi439@gmail.com>
2023-05-17 15:27:54 +0700

    push FE Auth

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -19,6 +19,11 @@
     <link rel="stylesheet" href="../assets/landing-page/css/style.default.css" id="theme-stylesheet">
     <!-- Custom stylesheet - for your changes-->
     <link rel="stylesheet" href="../assets/landing-page/css/custom.css">
+    <!-- MATERIAL DESIGN ICONIC FONT -->
+		<link rel="stylesheet" href="../auth/fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
+		
+		<!-- STYLE CSS -->
+		<link rel="stylesheet" href="../auth/css/style.css">
     <!-- Favicon-->
     <link rel="shortcut icon" href="../assets/landing-page/img/imig.png">
   </head>
@@ -38,16 +43,67 @@
       </nav>
     </header>
     <!-- Hero Section-->
-    <section class="hero bg-top py-5" id="hero" style="background: url(../assets/landing-page/img/banner-4.png) no-repeat; background-size: 100% 80%">
+    <section class="hero bg-top py-5" id="hero" style="background: url(../assets/landing-page/img/banner-4.png) no-repeat;background-size: 100% 80%">
       <div class="container py-5">
         <div class="row py-5">
-          <div class="col-lg-5 py-5">
+          <div class="col">
             @yield('content')
           </div>
           
         </div>
       </div>
     </section>
+    <footer class="with-pattern-1 position-relative pt-5" style="background-color:#274472">
+      <div class="container py-5">
+        <div class="row gy-4">
+          <div class="col-lg-3"><img class="mb-4" src="../assets/landing-page/img/imig.png" alt="" width="110">
+            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+          </div>
+          <div class="col-lg-2">
+            <h2 class="h5 mb-4">Quick Links</h2>
+            <div class="d-flex">
+              <ul class="list-unstyled d-inline-block me-4 mb-0">
+                <li class="mb-2"><a class="footer-link" href="#!">History</a></li>
+                <li class="mb-2"><a class="footer-link" href="#!">About us</a></li>
+                <li class="mb-2"><a class="footer-link" href="#!">Contact us</a></li>
+                <li class="mb-2"><a class="footer-link" href="#!">Services</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-2">
+            <h2 class="h5 mb-4">Services</h2>
+            <div class="d-flex">
+              <ul class="list-unstyled me-4 mb-0">
+                <li class="mb-2"><a class="footer-link" href="#!">History</a></li>
+                <li class="mb-2"><a class="footer-link" href="#!">About us</a></li>
+                <li class="mb-2"><a class="footer-link" href="#!">Contact us</a></li>
+                <li class="mb-2"><a class="footer-link" href="#!">Services</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-5">
+            <h2 class="h5 mb-4">Contact Info</h2>
+            <ul class="list-unstyled me-4 mb-3">
+              <li class="mb-2 text-muted">728  Ocello Street, San Diego, California. </li>
+              <li class="mb-2"><a class="footer-link" href="tel:619-851-4132">619-851-4132</a></li>
+              <li class="mb-2"><a class="footer-link" href="mailto:Nova@example.com">Nova@example.com</a></li>
+            </ul>
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item"><a class="social-link" href="#!"><i class="fab fa-facebook-f"></i></a></li>
+              <li class="list-inline-item"><a class="social-link" href="#!"><i class="fab fa-twitter"></i></a></li>
+              <li class="list-inline-item"><a class="social-link" href="#!"><i class="fab fa-linkedin-in"></i></a></li>
+              <li class="list-inline-item"><a class="social-link" href="#!"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="copyrights">       
+        <div class="container text-center py-4">
+          <p class="mb-0 text-muted text-sm">&copy; 2021, Your company. Template by <a href="https://bootstrapious.com/p/app-landing-page" class="text-reset">Bootstrapious</a>.</p>
+          <!-- If you want to remove the backlink, please purchase the Attribution-Free License. See details in readme.txt or license.txt. Thanks!-->
+        </div>
+      </div>
+    </footer>
     <!-- JavaScript files-->
     <script src="../assets/landing-page/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="../assets/landing-page/vendor/swiper/swiper-bundle.min.js"></script>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,54 +1,39 @@
 @extends('auth.layout')
 @section('content')
-
-<div class="container">
-    <div class="row justify-content-center">
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-md-12 col-lg-10">
-            <div class="">
-                
-                <div class="login-wrap p-4 p-lg-5">
-                    <div class="d-flex">
-                        <div class="w-100">
-                            <h3 class="mb-4">Sign In</h3>
-                        </div>
-                        <div class="w-100">
-                            <p class="social-media d-flex justify-content-end">
-                                <a href="#" class="social-icon d-flex align-items-center justify-content-center"><span
-                                        class="fa fa-facebook"></span></a>
-                                <a href="#" class="social-icon d-flex align-items-center justify-content-center"><span
-                                        class="fa fa-twitter"></span></a>
-                            </p>
-                        </div>
-                    </div>
-                    <form action="#" class="signin-form">
-                        <div class="form-group mb-3">
-                            <label class="label" for="name">Username</label>
-                            <input type="text" class="form-control" placeholder="Username" required>
-                        </div>
-                        <div class="form-group mb-3">
-                            <label class="label" for="password">Password</label>
-                            <input type="password" class="form-control" placeholder="Password" required>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
-                        </div>
-                        <div class="form-group d-md-flex">
-                            <div class="w-50 text-left">
-                                <label class="checkbox-wrap checkbox-primary mb-0">Remember Me
-                                    <input type="checkbox" checked>
-                                    <span class="checkmark"></span>
-                                </label>
-                            </div>
-                            <div class="w-50 text-md-right">
-                                <a href="#">Forgot Password</a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<div class="wrappe mt-4 text-align-center" style="background-image: url('images/bg-registration-form-2.jpg');">
+			<div class="inner mt-3">
+				<form action="">
+					<h3>Registration Form</h3>
+					<div class="form-group">
+						<div class="form-wrapper">
+							<label for="">First Name</label>
+							<input type="text" class="form-control">
+						</div>
+						<div class="form-wrapper">
+							<label for="">Last Name</label>
+							<input type="text" class="form-control">
+						</div>
+					</div>
+					<div class="form-wrapper">
+						<label for="">Email</label>
+						<input type="text" class="form-control">
+					</div>
+					<div class="form-wrapper">
+						<label for="">Password</label>
+						<input type="password" class="form-control">
+					</div>
+					<div class="form-wrapper">
+						<label for="">Confirm Password</label>
+						<input type="password" class="form-control">
+					</div>
+					<div class="checkbox">
+						<label>
+							<input type="checkbox"> I caccept the Terms of Use & Privacy Policy.
+							<span class="checkmark"></span>
+						</label>
+					</div>
+					<button>Register Now</button>
+				</form>
+			</div>
+		</div>
 @endsection
