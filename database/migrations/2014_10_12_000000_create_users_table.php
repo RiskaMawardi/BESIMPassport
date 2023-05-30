64432509865bd28e2ed1682df97e56344e550fb1
RiskaMawardi <riskamawardi439@gmail.com>
2023-05-30 17:22:26 +0700

    p

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('no_hp');
             $table->string('no_kk');
+
             $table->boolean('role')->default(0);
             $table->rememberToken();
             $table->timestamps();
